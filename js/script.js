76c68b8e58c3daff182941f792c0c4db49387902
unknown <kamila.wilczynska92@gmail.com>
2022-09-01 11:32:03 +0100

    Add variable playerMove in js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
+let playerMove = `papier`;
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
